47be6e87e91eb622dacc0d3bafbc251cec8b93ec
zhanlidegui <1442334243@qq.com>
2024-12-27 20:21:07 +0800

    修改移动方式

--- lklk/src/sprite/sprite_move.rs
+++ lklk/src/sprite/sprite_move.rs
@@ -4,39 +4,39 @@ use bevy::prelude::*;
 
 use crate::sprite_player::*;
 
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
+// #[derive(Component)]
+// enum Direction {
+//     Up,
+//     Down,
+//     Left,
+//     Right,
     
-}
-fn sprite_move(time:Res<Time>,mut sprite_position:Query<(&mut Direction,&mut Transform,&mut Sprite)>)
-{
-    for (mut logo,mut transform,mut sprite) in &mut sprite_position 
-    {
-        match *logo {
-            Direction::Up => transform.translation.y += 150. * time.delta_secs(),
-            Direction::Down => transform.translation.y -= 150. * time.delta_secs(),
-            Direction::Left => transform.translation.x -= 150. * time.delta_secs(),
-            Direction::Right => transform.translation.x += 150. * time.delta_secs(),
-        }
-        if transform.translation.x > 400. {
-            *logo = Direction::Left;
-            sprite.flip_x = true;
-        }else if transform.translation.x < -400. {
-            *logo = Direction::Right;
-            sprite.flip_x = false;
-        }
+// }
+// fn sprite_move(time:Res<Time>,mut sprite_position:Query<(&mut Direction,&mut Transform,&mut Sprite)>)
+// {
+//     for (mut logo,mut transform,mut sprite) in &mut sprite_position 
+//     {
+//         match *logo {
+//             Direction::Up => transform.translation.y += 150. * time.delta_secs(),
+//             Direction::Down => transform.translation.y -= 150. * time.delta_secs(),
+//             Direction::Left => transform.translation.x -= 150. * time.delta_secs(),
+//             Direction::Right => transform.translation.x += 150. * time.delta_secs(),
+//         }
+//         if transform.translation.x > 400. {
+//             *logo = Direction::Left;
+//             sprite.flip_x = true;
+//         }else if transform.translation.x < -400. {
+//             *logo = Direction::Right;
+//             sprite.flip_x = false;
+//         }
 
-        if transform.translation.y > 400. {
-            transform.translation.y = -400.;
-        }else if transform.translation.y < -400. {
-            transform.translation.y = 400.;
-        }
-    }
-}
+//         if transform.translation.y > 400. {
+//             transform.translation.y = -400.;
+//         }else if transform.translation.y < -400. {
+//             transform.translation.y = 400.;
+//         }
+//     }
+// }
     
 
 
@@ -47,7 +47,12 @@ impl Plugin for SpriteMovePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
             .add_systems(Update, animation_sprite)
-            .add_systems(Update,sprite_move);
+            .add_systems(FixedUpdate, advance_physics)
+            .add_systems(RunFixedMainLoop,
+            (
+                handle_input.in_set(RunFixedMainLoopSystem::BeforeFixedMainLoop),
+                interpolate_rendered_transform.in_set(RunFixedMainLoopSystem::AfterFixedMainLoop),
+            ));
     }
 }
 
@@ -64,6 +69,7 @@ fn setup(mut commands: Commands,asset_server:Res<AssetServer>,mut texture_atlas_
 
     commands.spawn(Camera2d);
     commands.spawn((
+        Name::new("Player"),
         Sprite::from_atlas_image(
             texture,
             TextureAtlas{
@@ -74,7 +80,11 @@ fn setup(mut commands: Commands,asset_server:Res<AssetServer>,mut texture_atlas_
         Transform::from_scale(Vec3::splat(6.0)),
         animation_indices,
         AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-        Direction::Right,
+        AccumulatedInput::default(),
+        Velocity::default(),
+        PhysicalTranslation::default(),
+        PreviousPhysicalTranslation::default(),
+
     ));
 }
 
@@ -91,8 +101,92 @@ struct PhysicalTranslation(Vec3);
 #[derive(Debug,Component,Clone,Copy,PartialEq,Default,Deref,DerefMut)]
 struct PreviousPhysicalTranslation(Vec3);
 
-fn spawn_player(mut commands:Commands,asset_server:Res<AssetServer>)
+// fn spawn_player(mut commands:Commands,asset_server:Res<AssetServer>)
+// {
+//     commands.spawn(Camera2d);
+//     commands.spawn((
+//         Name::new("Player"),
+//         Sprite::from_image(asset_server.load("textures/rpg/chars/gabe/gabe-idle-run.png")),
+//         Transform::from_scale(Vec3::splat(0.3)),
+//         AccumulatedInput::default(),
+//         Velocity::default(),
+//         PhysicalTranslation::default(),
+//         PreviousPhysicalTranslation::default(),
+//     )); 
+// }
+fn handle_input(
+    keyboard_input:Res<ButtonInput<KeyCode>>,
+    mut query:Query<(&mut AccumulatedInput,&mut Velocity,&mut Sprite)>
+)
 {
-    commands.spawn(Camera2d);
-    
+    const SPEED:f32 = 210.0;
+    for(mut input,mut velocity,mut sprite) in query.iter_mut()
+    {
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            input.0.y += 1.0;
+
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            input.0.y -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            input.0.x -= 1.0;
+            sprite.flip_x = true;
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            input.0.x += 1.0;
+            sprite.flip_x = false;
+        }
+
+
+        velocity.0 = input.extend(0.0).normalize_or_zero() * SPEED;
+    }
+}
+
+fn advance_physics(
+    fixed_time:Res<Time<Fixed>>,
+    mut query:Query<(
+        &mut PhysicalTranslation,
+        &mut PreviousPhysicalTranslation,
+        &mut AccumulatedInput,
+        &Velocity,
+    )>,
+)
+{
+    for(mut current_pyhsical_translation,
+        mut previous_physical_translation,
+        mut input,
+        velocity,
+    ) in query.iter_mut()
+    {
+        previous_physical_translation.0 = current_pyhsical_translation.0;
+        current_pyhsical_translation.0 += velocity.0 *fixed_time.delta_secs();
+
+        input.0 = Vec2::ZERO;
+    }
+}
+
+fn interpolate_rendered_transform(
+    fixed_time:Res<Time<Fixed>>,
+    mut query:Query<(
+        &mut Transform,
+        &PhysicalTranslation,
+        &PreviousPhysicalTranslation
+    )>
+)
+{
+    for (mut transform,
+        current_physical_translation,
+        previous_physical_translation
+    ) in query.iter_mut()
+    {
+        let previous = previous_physical_translation.0;
+        let current = current_physical_translation.0;
+
+        let alpha = fixed_time.overstep_fraction();
+
+        let rendered_translation = previous.lerp(current,alpha);
+
+        transform.translation = rendered_translation;
+    }
 }
